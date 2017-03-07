67bf7c54f2e80af6ecb9a973f63d1bbaab07a705
David Henningsson <diwic@ubuntu.com>
2017-03-07 06:42:51 +0100

    Add a new properties example using stdintf.
    Signed-off-by: David Henningsson <diwic@ubuntu.com>

--- examples/properties.rs
+++ examples/properties.rs
@@ -1,10 +1,29 @@
 extern crate dbus;
 
-use dbus::{Connection, BusType, Props};
+use dbus::{Connection, BusType, stdintf};
 
 fn main() {
-    let c = Connection::get_private(BusType::System).unwrap();
-    let p = Props::new(&c, "org.freedesktop.PolicyKit1", "/org/freedesktop/PolicyKit1/Authority",
-        "org.freedesktop.PolicyKit1.Authority", 10000);
-    println!("BackendVersion: {:?}", p.get("BackendVersion").unwrap())
+    // Connect to server and create a ConnPath. A ConnPath implements several interfaces,
+    // in this case we'll use OrgFreedesktopDBusProperties, which allows us to call "get".
+    let c = Connection::get_private(BusType::Session).unwrap();
+    let p = c.with_path("org.mpris.MediaPlayer2.rhythmbox", "/org/mpris/MediaPlayer2", 5000);
+    use stdintf::OrgFreedesktopDBusProperties;
+    let metadata = p.get("org.mpris.MediaPlayer2.Player", "Metadata").unwrap();
+
+    // The Metadata property is a Dict<String, Variant>, we can get the values out by iterating over it.
+    // When using "as_iter()" for a dict, we'll get one key, it's value, next key, it's value, etc.
+    // The ".0" is needed to traverse into the variant.   
+    let mut iter = metadata.0.as_iter().unwrap();
+
+    while let Some(key) = iter.next() {
+        // Printing the key is easy, since we know it's a String.
+        print!("{}: ", key.as_str().unwrap());
+
+        // We don't know what type the value is. We'll try a few and fall back to
+        // debug printing if the value is more complex than that.
+        let value = iter.next().unwrap();
+        if let Some(s) = value.as_str() { println!("{}", s); }
+        else if let Some(i) = value.as_i64() { println!("{}", i); }
+        else { println!("{:?}", value); }
+    }
 }
