b6c02346c3a12549f7d08496168a1d86c422c06e
David Henningsson <diwic@ubuntu.com>
2017-06-24 19:37:39 +0200

    Tokio: Add ConnectionItems iterator
    Signed-off-by: David Henningsson <diwic@ubuntu.com>

--- async/dbus-tokio/src/adriver.rs
+++ async/dbus-tokio/src/adriver.rs
@@ -6,7 +6,7 @@ use mio::unix::UnixReady;
 use std::{io, mem, fmt};
 use dbus::{Connection, ConnectionItems, ConnectionItem, Watch, WatchEvent, MsgHandler, Message, Error as DBusError};
 use futures::{Async, Future, task, Stream, Poll};
-use futures::sync::oneshot;
+use futures::sync::{oneshot, mpsc};
 use tokio_core::reactor::{PollEvented, Handle as CoreHandle};
 use std::rc::{Rc, Weak};
 use std::os::raw::c_uint;
@@ -16,28 +16,34 @@ use std::os::unix::io::RawFd;
 
 type MCallMap = Rc<RefCell<HashMap<u32, oneshot::Sender<Message>>>>;
 
+type CItems = Rc<RefCell<Option<mpsc::UnboundedSender<ConnectionItem>>>>;
+
 #[derive(Debug)]
 pub struct AConnection {
     conn: Rc<Connection>,
     quit: Option<oneshot::Sender<()>>,
     callmap: MCallMap,
+    itemstream: CItems,
 }
 
 impl AConnection {
     pub fn new(c: Rc<Connection>, h: CoreHandle) -> io::Result<AConnection> {
         let (tx, rx) = oneshot::channel();
         let map: MCallMap = Default::default();
+        let istream: CItems = Default::default();
         let mut d = ADriver {
             conn: c.clone(),
             fds: HashMap::new(),
             core: h.clone(),
             quit: rx,
             callmap: map.clone(),
+            itemstream: istream.clone(),
         };
         let i = AConnection {
             conn: c,
             quit: Some(tx),
             callmap: map,
+            itemstream: istream,
         };
         for w in i.conn.watch_fds() { d.modify_watch(w)?; }
         h.spawn(d);
@@ -52,6 +58,14 @@ impl AConnection {
         let mc = AMethodCall { serial: r, callmap: self.callmap.clone(), inner: rx };
         Ok(mc)
     }
+
+    pub fn items(&self) -> Result<AConnectionItems, &'static str> {
+        let mut i = self.itemstream.borrow_mut();
+        if i.is_some() { return Err("Another instance of AConnectionItems already exists"); }
+        let (tx, rx) = mpsc::unbounded();
+        *i = Some(tx);
+        Ok(AConnectionItems { inner: rx, stream: self.itemstream.clone() })
+    }
 }
 
 impl Drop for AConnection {
@@ -65,6 +79,7 @@ struct ADriver {
     core: CoreHandle,
     quit: oneshot::Receiver<()>,
     callmap: MCallMap,
+    itemstream: CItems,
 }
 
 impl ADriver {
@@ -80,24 +95,31 @@ impl ADriver {
             self.fds.remove(&w.fd());
 
             let z = PollEvented::new(AWatch(w), &self.core)?;
+
             self.fds.insert(w.fd(), z);
         }
         Ok(())
     }
 
+    fn send_stream(&self, m: ConnectionItem) {
+        self.itemstream.borrow().as_ref().map(|z| z.send(m).unwrap());
+    }
+
     fn handle_items(&mut self, items: ConnectionItems) {
         // TODO: What about all unwrapping in this function
         for i in items {
-            println!("Got: {:?}", i);
+            // println!("Got: {:?}", i);
             match i {
-                ConnectionItem::WatchFd(w) => self.modify_watch(w).unwrap(),
+                ConnectionItem::WatchFd(w) => { self.modify_watch(w).unwrap(); },
                 ConnectionItem::MethodReturn(m) => {
                     let mut map = self.callmap.borrow_mut();
                     let serial = m.get_reply_serial().unwrap();
                     let r = map.remove(&serial);
-                    if let Some(r) = r { r.send(m).unwrap(); };
+                    if let Some(r) = r { r.send(m).unwrap(); }
+                    else { self.send_stream(ConnectionItem::MethodReturn(m)) }
                 }
-                _ => {},
+                ConnectionItem::Nothing => return,
+                i @ _ => self.send_stream(i),
             }
         }
     }
@@ -118,18 +140,19 @@ impl Future for ADriver {
             if w.get_ref().0.readable() { mask = mask | Ready::readable().into(); }
             if w.get_ref().0.writable() { mask = mask | Ready::writable().into(); }
             let pr = w.poll_ready(*mask);
+            // println!("{:?} is {:?}", w.get_ref().0.fd(), pr);
             let ur = if let Async::Ready(t) = pr { UnixReady::from(t) } else { continue };
             let flags =
                 if ur.is_readable() { WatchEvent::Readable as c_uint } else { 0 } +
                 if ur.is_writable() { WatchEvent::Writable as c_uint } else { 0 } +
                 if ur.is_hup() { WatchEvent::Hangup as c_uint } else { 0 } +
                 if ur.is_error() { WatchEvent::Error as c_uint } else { 0 };
-            println!("{:?} is {:?}", w.get_ref().0.fd(), ur);
+            // println!("{:?} is {:?}", w.get_ref().0.fd(), ur);
             items = Some(cc.watch_handle(w.get_ref().0.fd(), flags));
             if ur.is_readable() { w.need_read() };
             if ur.is_writable() { w.need_write() };
         };
-        if let Some(items) = items { self.handle_items(items) };
+        self.handle_items(items.unwrap_or(cc.iter(0)));
         Ok(Async::NotReady)
     }
 }
@@ -183,6 +206,33 @@ impl Future for AMethodCall {
     }
 }
 
+impl Drop for AMethodCall {
+    fn drop(&mut self) {
+        let mut map = self.callmap.borrow_mut();
+        map.remove(&self.serial);
+    }
+}
+
+#[derive(Debug)]
+pub struct AConnectionItems {
+    inner: mpsc::UnboundedReceiver<ConnectionItem>,
+    stream: CItems,
+}
+
+impl Stream for AConnectionItems {
+    type Item = ConnectionItem;
+    type Error = ();
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        self.inner.poll()
+    }
+}
+
+impl Drop for AConnectionItems {
+    fn drop(&mut self) {
+        *self.stream.borrow_mut() = None;
+    }
+}
+
 #[test]
 fn aconnection_test() {
     let conn = Rc::new(Connection::get_private(::dbus::BusType::Session).unwrap());
@@ -193,5 +243,17 @@ fn aconnection_test() {
     let reply = core.run(aconn.method_call(m).unwrap()).unwrap();
     let z: Vec<&str> = reply.get1().unwrap();
     println!("got reply: {:?}", z);
+}
+
+#[test]
+fn astream_test() {
+    let conn = Rc::new(Connection::get_private(::dbus::BusType::Session).unwrap());
+    let mut core = ::tokio_core::reactor::Core::new().unwrap();
+    let aconn = AConnection::new(conn.clone(), core.handle()).unwrap();
+
+    let items: AConnectionItems = aconn.items().unwrap();
+    let signals = items.filter_map(|m| if let ConnectionItem::Signal(m) = m { Some(m) } else { None });
+    let firstsig = core.run(signals.into_future()).map(|(x, _)| x).map_err(|(x, _)| x).unwrap();
+    println!("first signal was: {:?}", firstsig);
 }
 
