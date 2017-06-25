a59c2b92326e772db71519ba7e0f6506d151d8f4
David Henningsson <diwic@ubuntu.com>
2017-06-25 08:03:32 +0200

    Add doc comment for tokio crate
    Signed-off-by: David Henningsson <diwic@ubuntu.com>

--- async/dbus-tokio/src/lib.rs
+++ async/dbus-tokio/src/lib.rs
@@ -3,9 +3,17 @@ extern crate futures;
 extern crate tokio_core;
 extern crate mio;
 
-/// Tokio integration for dbus
-///
-/// For examples to get you started, see the examples directory and the Readme.
+//! Tokio integration for dbus
+//!
+//! What's currently working is:
+//!
+//!  * Client: Make method calls and wait asynchronously for them to be replied to - see `AConnection::method_call`
+//!  * Get a stream of incoming messages (so you can listen to signals etc) - see `AConnection::messages`
+//!  * Server: Make a tree handle that stream of incoming messages - see `tree::ATreeServer`
+//!  * Server: Add asynchronous methods to the tree - in case you cannot reply right away,
+//!    you can return a future that will reply when that future resolves - see `tree::AFactory::amethod`
+//! 
+//! For examples to get you started, see the examples directory and the Readme.
 
 pub mod tree;
 
