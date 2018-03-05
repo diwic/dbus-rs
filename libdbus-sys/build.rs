5286c6275664a8073c5fbc3dab6286e2907c7515
David Henningsson <diwic@ubuntu.com>
2018-03-05 11:15:32 +0100

    libdbus-sys: Depend on pkg_config rather than metadeps
    ...but keep the metadeps key in Cargo.toml.
    Hopefully that still allows debcargo to work.
    
    Closes: https://github.com/diwic/dbus-rs/pull/111
    
    Suggested-by: Bastien Orivel <eijebong@bananium.fr>

--- libdbus-sys/build.rs
+++ libdbus-sys/build.rs
@@ -1,5 +1,7 @@
-extern crate metadeps;
+extern crate pkg_config;
 
 fn main() {
-    metadeps::probe().unwrap();
+    // See https://github.com/joshtriplett/metadeps/issues/9 for why we don't use
+    // metadeps here, but instead keep this manually in sync with Cargo.toml.
+    pkg_config::Config::new().atleast_version("1.6").probe("dbus-1").unwrap();
 }
