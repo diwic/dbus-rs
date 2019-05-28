11f8c921ff36ccc06098cb0034dd0780fa8f3527
David Henningsson <diwic@ubuntu.com>
2019-05-28 21:01:35 +0200

    Update example to not use ConnectionItem

--- dbus/examples/unity_focused_window.rs
+++ dbus/examples/unity_focused_window.rs
@@ -4,13 +4,13 @@ extern crate dbus;
 // FocusedWindowChanged signal. The signal contains "window_id", "app_id" and "stage",
 // we print only "app_id". 
 
-use dbus::connection::{Connection, BusType, ConnectionItem};
+use dbus::{Connection, BusType, Message, MessageType};
 
-fn focus_msg(ci: &ConnectionItem) -> Option<&str> {
-    let m = if let &ConnectionItem::Signal(ref s) = ci { s } else { return None };
-    if &*m.interface().unwrap() != "com.canonical.Unity.WindowStack" { return None };
-    if &*m.member().unwrap() != "FocusedWindowChanged" { return None };
-    let (_, app) = m.get2::<u32, &str>();
+fn focus_msg(msg: &Message) -> Option<&str> {
+    if msg.msg_type() != MessageType::Signal { return None };
+    if &*msg.interface().unwrap() != "com.canonical.Unity.WindowStack" { return None };
+    if &*msg.member().unwrap() != "FocusedWindowChanged" { return None };
+    let (_, app) = msg.get2::<u32, &str>();
     app
 }
 
@@ -18,8 +18,12 @@ fn main() {
     let c = Connection::get_private(BusType::Session).unwrap();
     c.add_match("interface='com.canonical.Unity.WindowStack',member='FocusedWindowChanged'").unwrap();
 
-    for i in c.iter(1000) {
-        if let Some(app) = focus_msg(&i) { println!("{} has now focus.", app) };
+    loop {
+        if let Some(msg) = c.incoming(1000).next() {
+            if let Some(app) = focus_msg(&msg) {
+                println!("{} has now focus.", app);
+            }
+        }
     }
 }
 
