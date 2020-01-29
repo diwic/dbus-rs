6a19f0afb08a28f6ff115e2200033a44ca63f7c7
David Henningsson <diwic@ubuntu.com>
2020-01-29 21:34:51 +0100

    native: Add address finding

---
+++ dbus-native/src/address.rs
@@ -0,0 +1,32 @@
+pub fn read_session_address() -> Result<String, Box<dyn std::error::Error>> {
+    for (key, value) in std::env::vars_os() {
+        if key == "DBUS_SESSION_BUS_ADDRESS" {
+            if let Ok(v) = value.into_string() { return Ok(v) }
+        }
+    }
+    // TODO: according to the D-Bus spec, there are more ways to find the address, such
+    // as asking the X window system.
+    Err("Environment variable not found")?
+}
+
+pub fn read_system_address() -> Result<String, Box<dyn std::error::Error>> {
+    for (key, value) in std::env::vars_os() {
+        if key == "DBUS_SYSTEM_BUS_ADDRESS" {
+            if let Ok(v) = value.into_string() { return Ok(v) }
+        }
+    }
+    Ok("unix:path=/var/run/dbus/system_bus_socket".into())
+}
+
+#[test]
+fn bus_exists() {
+    let addr = read_session_address().unwrap();
+    assert!(addr.starts_with("unix:path="));
+    let path = std::path::Path::new(&addr["unix:path=".len()..]);
+    assert!(path.exists());
+
+    let addr = read_system_address().unwrap();
+    assert!(addr.starts_with("unix:path="));
+    let path = std::path::Path::new(&addr["unix:path=".len()..]);
+    assert!(path.exists());
+}
--- dbus-native/src/lib.rs
+++ dbus-native/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod strings;
 
 pub mod machineid;
+
+pub mod address;
