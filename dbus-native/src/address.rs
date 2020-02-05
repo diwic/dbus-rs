8eebf35e5715a27fbca6817fdee582830dc8479d
David Henningsson <diwic@ubuntu.com>
2020-02-05 08:46:08 +0100

    native: Improve address parsing

--- dbus-native/src/address.rs
+++ dbus-native/src/address.rs
@@ -1,36 +1,36 @@
 use std::os::unix::net::UnixStream;
 
-pub fn read_session_address() -> Result<String, Box<dyn std::error::Error>> {
-    for (key, value) in std::env::vars_os() {
-        if key == "DBUS_SESSION_BUS_ADDRESS" {
-            if let Ok(v) = value.into_string() { return Ok(v) }
+fn env_key(key: &str) -> Option<String> {
+    for (akey, value) in std::env::vars_os() {
+        if akey == key {
+            if let Ok(v) = value.into_string() { return Some(v) }
         }
     }
+    None
+}
+
+pub fn read_session_address() -> Result<String, Box<dyn std::error::Error>> {
+    Ok(env_key("DBUS_SESSION_BUS_ADDRESS").ok_or_else(|| "Environment variable not found")?)
     // TODO: according to the D-Bus spec, there are more ways to find the address, such
     // as asking the X window system.
-    Err("Environment variable not found")?
 }
 
 pub fn read_system_address() -> Result<String, Box<dyn std::error::Error>> {
-    for (key, value) in std::env::vars_os() {
-        if key == "DBUS_SYSTEM_BUS_ADDRESS" {
-            if let Ok(v) = value.into_string() { return Ok(v) }
-        }
-    }
-    Ok("unix:path=/var/run/dbus/system_bus_socket".into())
+    Ok(env_key("DBUS_SYSTEM_BUS_ADDRESS").unwrap_or_else(||
+        "unix:path=/var/run/dbus/system_bus_socket".into()
+    ))
 }
 
-pub fn address_to_sockaddr(s: &str) -> Result<libc::sockaddr_un, Box<dyn std::error::Error>> {
+pub fn read_starter_address() -> Result<String, Box<dyn std::error::Error>> {
+    Ok(env_key("DBUS_SESSION_BUS_ADDRESS").ok_or_else(|| "Environment variable not found")?)
+}
+
+fn make_sockaddr_un(start: usize, s: &str) -> Result<libc::sockaddr_un, Box<dyn std::error::Error>> {
+    let bytes = s.as_bytes();
     let mut r = libc::sockaddr_un {
         sun_family: libc::AF_UNIX as libc::sa_family_t,
         sun_path: [0; 108],
     };
-    let (start, bytes) = if s.starts_with("unix:path=") {
-        (0, &s.as_bytes()["unix:path=".len()..])
-    } else if s.starts_with("unix:abstract=") {
-        (1, &s.as_bytes()["unix:abstract=".len()..])
-    } else { Err(format!("unsupported address type: {}", s))? };
-
     if start+bytes.len()+1 >= r.sun_path.len() { Err("Address too long")? }
     for (i, &x) in bytes.into_iter().enumerate() {
         r.sun_path[i+start] = x as libc::c_char;
@@ -38,8 +38,22 @@ pub fn address_to_sockaddr(s: &str) -> Result<libc::sockaddr_un, Box<dyn std::er
     Ok(r)
 }
 
+pub fn address_to_sockaddr_un(s: &str) -> Result<libc::sockaddr_un, Box<dyn std::error::Error>> {
+    if !s.starts_with("unix:") { Err("Address is not a unix socket")? };
+    for pair in s["unix:".len()..].split(',') {
+        let mut kv = pair.splitn(2, "=");
+        if let Some(key) = kv.next() {
+            if let Some(value) = kv.next() {
+                if key == "path" { return make_sockaddr_un(0, value); }
+                if key == "abstract" { return make_sockaddr_un(1, value); }
+            }
+        }
+    }
+    Err(format!("unsupported address type: {}", s))?
+}
+
 pub fn connect_blocking(addr: &str) -> Result<UnixStream, Box<dyn std::error::Error>> {
-    let sockaddr = address_to_sockaddr(addr)?;
+    let sockaddr = address_to_sockaddr_un(addr)?;
     crate::sys::connect_blocking(&sockaddr)
 }
 
