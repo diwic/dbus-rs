c74c0880f099067b75e7f63670a86e79008c9119
David Henningsson <diwic@ubuntu.com>
2020-02-27 21:46:42 +0100

    dbus: Migrate example from ffidisp to blocking connection

--- dbus/examples/monitor.rs
+++ dbus/examples/monitor.rs
@@ -1,10 +1,11 @@
-use dbus::ffidisp::Connection;
+use dbus::blocking::Connection;
 use dbus::message::MatchRule;
+use std::time::Duration;
 
 // This programs implements the equivalent of running the "dbus-monitor" tool
 fn main() {
     // First open up a connection to the session bus.
-    let conn = Connection::new_session().expect("D-Bus connection failed");
+    let mut conn = Connection::new_session().expect("D-Bus connection failed");
 
     // Second create a rule to match messages we want to receive; in this example we add no
     // further requirements, so all messages will match
@@ -12,13 +13,12 @@ fn main() {
     rule.eavesdrop = true; // this lets us eavesdrop on *all* session messages, not just ours
 
     // Start matching
-    conn.add_match(&rule.match_str()).expect("add_match failed");
+    conn.add_match(rule, |_: (), _, msg| {
+        println!("{:?}", msg);
+        true
+    }).expect("add_match failed");
 
     // Loop and print out all messages received as they come.
     // Some can be quite large, e.g. if they contain embedded images..
-    loop {
-        if let Some(msg) = conn.incoming(5000).next() {
-            println!("{:?}", msg);
-        }
-    }
+    loop { conn.process(Duration::from_millis(1000)).unwrap(); };
 }
