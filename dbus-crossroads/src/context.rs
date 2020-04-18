36c3f5de5dbafcd70e28b7e826cbd51e7328c5f9
David Henningsson <diwic@ubuntu.com>
2020-04-18 20:54:56 +0200

    Crossroads: Minor work on context

--- dbus-crossroads/src/context.rs
+++ dbus-crossroads/src/context.rs
@@ -17,12 +17,12 @@ enum MsgCow<'a> {
 
 #[derive(Debug)]
 pub struct MsgCtx<'a> {
-    msg: MsgCow<'a>,
+    msg: Option<MsgCow<'a>>,
     member: MemberName<'a>,
-    iface: IfaceName<'a>,
+    iface: Option<IfaceName<'a>>,
     path: PathName<'a>,
     pub (super) signals: DBusSignals,
-
+    pub (super) reply: Option<Message>,
     pub (super) send_extra: Vec<Message>,
 }
 
@@ -30,9 +30,17 @@ impl MsgCtx<'static> {
     pub fn new(msg: Message) -> Option<Self> {
         if msg.msg_type() != MessageType::MethodCall { return None };
         let path = msg.path()?.into_static();
-        let iface = msg.interface()?.into_static();
+        let iface = msg.interface().map(|x| x.into_static());
         let member = msg.member()?.into_static();
-        Some(MsgCtx { msg: MsgCow::Owned(msg), member, iface, path, send_extra: vec!(), signals: Default::default() })
+        Some(MsgCtx { msg: Some(MsgCow::Owned(msg)), member, iface, path,
+            send_extra: vec!(),
+            signals: Default::default(),
+            reply: None
+        })
+    }
+
+    pub fn new_without_message(path: PathName<'static>, iface: Option<IfaceName<'static>>, member: MemberName<'static>) -> Self {
+        MsgCtx { msg: None, member, iface, path, send_extra: vec!(), signals: Default::default(), reply: None }
     }
 }
 
@@ -41,24 +49,33 @@ impl<'a> MsgCtx<'a> {
 
     pub fn message(&self) -> &Message {
         match self.msg {
-            MsgCow::Owned(ref r) => r,
-            MsgCow::Borrowed(r) => r,
+            Some(MsgCow::Owned(ref r)) => r,
+            Some(MsgCow::Borrowed(r)) => r,
+            None => panic!("No message"),
         }
     }
 
     pub fn member(&self) -> &MemberName<'a> { &self.member }
     pub fn path(&self) -> &PathName<'a> { &self.path }
-    pub fn interface(&self) -> &IfaceName<'a> { &self.iface }
+    // FIXME
+    pub fn interface(&self) -> &IfaceName<'a> { self.iface.as_ref().unwrap() }
 
     pub fn send_msg(&mut self, msg: Message) { self.send_extra.push(msg); }
 
     pub fn make_signal<'b, A: AppendAll, N: Into<MemberName<'b>>>(&self, name: N, args: A) -> Message {
-        let mut msg = Message::signal(&self.path, &self.iface, &name.into());
+        let mut msg = Message::signal(&self.path, self.iface.as_ref().unwrap(), &name.into());
         args.append(&mut IterAppend::new(&mut msg));
         msg
     }
 
     pub fn dbus_signals_mut(&mut self) -> &mut DBusSignals { &mut self.signals }
+
+    pub (crate) fn do_reply<F: FnOnce(&mut Message, &mut Self)>(&mut self, f: F) {
+        if self.message().get_no_reply() { return; }
+        let mut m = self.message().method_return();
+        f(&mut m, self);
+        self.reply = Some(m);
+    }
 }
 
 pub struct AsyncMsgCtx {
@@ -101,8 +118,8 @@ impl<'a, H: Handlers> RefCtx<'a, H> {
 
     pub (super) fn new<'b>(cr: &'a Crossroads<H>, ctx: &'b MsgCtx) -> Result<Self, MethodErr> {
         let path = cr.paths.get(ctx.path.as_cstr()).ok_or_else(|| { MethodErr::no_path(&ctx.path) })?;
-        let entry = cr.reg.get(ctx.iface.as_cstr()).ok_or_else(|| { MethodErr::no_interface(&ctx.iface) })?;
-        let iface = path.get_from_typeid(entry.typeid).ok_or_else(|| { MethodErr::no_interface(&ctx.iface) })?;
+        let entry = cr.reg.get(ctx.iface.as_ref().unwrap().as_cstr()).ok_or_else(|| { MethodErr::no_interface(&ctx.iface.as_ref().unwrap()) })?;
+        let iface = path.get_from_typeid(entry.typeid).ok_or_else(|| { MethodErr::no_interface(&ctx.iface.as_ref().unwrap()) })?;
         Ok(RefCtx { crossroads: cr, path, iface, iinfo: &entry.info })
     }
 
